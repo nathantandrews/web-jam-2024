89ed8648b176753ed6bc1b4327b981a6d9349cc1
pratham2021 <prathamhebbar2021@gmail.com>
2024-11-19 22:32:55 -0800

    small fix

--- firebase.js
+++ firebase.js
@@ -4,16 +4,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
+process.env.APP_ID
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCn2d0GSyCNStNXtwAXMWY35mYBO1vP4Fg",
-  authDomain: "food-inventory-app-c2e2b.firebaseapp.com",
-  projectId: "food-inventory-app-c2e2b",
-  storageBucket: "food-inventory-app-c2e2b.firebasestorage.app",
-  messagingSenderId: "662786278923",
-  appId: "1:662786278923:web:6daf30160cf37bf155290e",
-  measurementId: "G-HR399GKGYF"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
